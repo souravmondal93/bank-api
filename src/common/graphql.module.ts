ceefcfbe3eb571ceda2a9d8c23d2529f61b85509
Sourav Mondal <souravrkl.gamefreak@gmail.com>
2023-02-25 03:07:57 +0000

    [ADD] graphl apollo sandbox

--- src/common/graphql.module.ts
+++ src/common/graphql.module.ts
@@ -2,12 +2,15 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
   ],
 })
